e44a45de0a03023e38614d2fc5d8053e77b534f1
Zack Golden <ZGoldenBlue@gmail.com>
2019-02-24 12:30:24 -0600

    Switching to react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar App-header">
-          <a className="navbar-brand" href="/">
-            <img src={logo} width="30" height="80" alt="Animal Points"/>
-            Animal Points
-          </a>
-        </nav>
+        <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+          <div className="container">
+            <div className="navbar-nav">        
+
+              <a className="navbar-brand" href="/">Animal Points</a>
+              <a className="nav-item nav-link" href="#home">Home</a>
+              <a className="nav-item nav-link" href="#teams">Teams</a>
+              <a className="nav-item nav-link" href="#help">Help</a>
+              <div className="dropdown">
+                {/*eslint-disable-next-line*/}
+                <a className="nav-item nav-link dropdown-toggle" 
+                data-toggle="dropdown" id="userDropdown"
+                aria-haspopup="true" aria-expanded="false"
+                href="#">User</a>
+                <div className="dropdown-menu" aria-labelledby="userDropdown">
+                  <a className="dropdown-item" href="#userProfile">Profile</a>
+                  <a className="dropdown-item" href="#userSettings">Settings</a>
+                  <a className="dropdown-item" href="#userLogOut">Log Out</a>
+                </div>
+              </div>
+            </div>  
+          </div>
+        </nav> {/*Navbar*/}
         <div className="container App-contentPane" >
-          hello
+            hello
         </div>
       </div>
     );
